f9f956df2ad0b570db8375480e4f0f3324dfc942
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2023-06-25 11:17:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,6 +61,7 @@ BUTTON_LOCK = is_enabled(environ.get("BUTTON_LOCK", "True"), True)
 # url shortner
 SHORT_URL = environ.get("SHORT_URL")
 SHORT_API = environ.get("SHORT_API")
+VERIFY = bool(environ.get('VERIFY', False))
 
 # Others
 IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
